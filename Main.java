3292da180652b0fb96b5e5f62e5cd50d394e0b6c
Geo <Geowong1991@yahoo.com>
2021-08-19 13:28:30 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		MainGame mainGame = new MainGame();
+
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Brick Breaker");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(mainGame);
+		obj.setVisible(true);
+	}
+}
